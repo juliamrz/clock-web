30049999b2910e6b41989e0c320ac95849e2e9c3
magna <julija@flashphoner.com>
2026-03-03 17:05:41 +0200

    added close driwer icon,

--- src/components/ui/Icons/MenuIcon/MenuIcon.tsx
+++ src/components/ui/Icons/MenuIcon/MenuIcon.tsx
@@ -21,11 +21,11 @@ const MenuIcon = (props: MenuIconProps) => {
       }}
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M8.5 10L23.5 10" stroke="#cccccc" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M8.5 5H23.5" stroke="#cccccc" stroke-linecap="round" stroke-linejoin="round"/>
-      <path d="M8.5 15H23.5" stroke="#cccccc" stroke-linecap="round" stroke-linejoin="round"/>
+      <path d="M8.5 10L23.5 10" stroke="#cccccc" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M8.5 5H23.5" stroke="#cccccc" strokeLinecap="round" strokeLinejoin="round"/>
+      <path d="M8.5 15H23.5" stroke="#cccccc" strokeLinecap="round" strokeLinejoin="round"/>
       {isOpen && (
-        <path d="M5.5 6.5L2 10L5.5 13.5" stroke="#cccccc" stroke-linecap="round" stroke-linejoin="round"/>
+        <path d="M5.5 6.5L2 10L5.5 13.5" stroke="#cccccc" strokeLinecap="round" strokeLinejoin="round"/>
       )}
     </svg>
   );
